93beab917c2fc7e0b14aeb9f80ddc4ef1e097dd6
Vallie Tracy <trac0055@umn.edu>
2021-10-20 12:02:01 -0500

    initializing repo

---
+++ static/js/coffeeShop.js
@@ -0,0 +1,53 @@
+"use strict";
+
+//adds item to data field to table row
+const addItemToCart = (itemName) => {
+  $('#cart-items').append(`
+    <tr>
+      <td>${itemName}</td>
+    </tr>
+  `);
+};
+
+//resets cart back to empty
+const resetCart = () => {
+  $('#cart-total').html('0.00');
+  $('#cart-items').empty();
+};
+
+//increments cart total 
+const incrementCartTotal = (price) => {
+  const cartTotal = $('#cart-total');
+
+  let total = Number(cartTotal.html());
+  total += price;
+
+  cartTotal.html(total.toFixed(2));
+};
+
+//increments num cups coffee sold
+const incrementCoffeeSold = (amountSold) => {
+  let coffeeSold = Number($('#coffee-sold-counter').html());
+  coffeeSold += amountSold;
+
+  $('#coffee-sold-counter').html(coffeeSold);
+};
+
+
+const setProgressAndStatus = (progressVal, statusMsg) => {
+  $('#order-progress').attr('value', progressVal);
+  $('#order-status-message').html(statusMsg);
+};
+
+
+//
+// Add your event handlers below.
+//
+$('.add-to-order').on('click', () => { 
+  addItemToCart('Coffee');
+  incrementCartTotal(1.50);
+});
+
+$('#place-order').on('click', () => {
+  incrementCoffeeSold($(''))
+})
